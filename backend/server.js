a3c4cd9cb093a254c8f3a8490651a88bda9b798f
Asjad Afzaal <132608659+asjad491@users.noreply.github.com>
2024-03-23 02:52:51 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,17 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
 
-
 const app = express();
-const port = 5000;
 
-
-// Enable CORS
 app.use(cors());
+app.use(bodyParser.json());
 
-// Create connection to MySQL database
 const db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: 'asjad123',
   database: 'expensedb'
 });
-
 
 db.connect((err) => {
   if (err) {
@@ -28,14 +23,9 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// Endpoint to save expenses to the database
 app.post('/api/save-expenses', (req, res) => {
   const { filename, expenses } = req.body;
 
-  // Insert expenses into database
   expenses.forEach(expense => {
     const { name, amount, date } = expense;
     const sql = `INSERT INTO expenses (expensename, amount, date, filename) VALUES (?, ?, ?, ?)`;
@@ -50,22 +40,21 @@ app.post('/api/save-expenses', (req, res) => {
   res.status(200).json({ message: 'Expenses saved successfully' });
 });
 
-// Endpoint to retrieve expenses from the database based on filename
 app.get('/api/get-expenses', (req, res) => {
-    const { filename } = req.query;
-    const sql = `SELECT * FROM expenses WHERE filename = ?`;
-    db.query(sql, [filename], (err, results) => {
-      if (err) {
-        console.error('Error retrieving expenses:', err);
-        return res.status(500).json({ error: 'An error occurred while retrieving expenses' });
-      }
-      console.log('Retrieved expenses:', results); // Log the retrieved data
-      res.status(200).json(results);
-    });
+  const { filename } = req.query;
+  const sql = `SELECT * FROM expenses WHERE filename = ?`;
+  db.query(sql, [filename], (err, results) => {
+    if (err) {
+      console.error('Error retrieving expenses:', err);
+      return res.status(500).json({ error: 'An error occurred while retrieving expenses' });
+    }
+    console.log('Retrieved expenses:', results);
+    res.status(200).json(results);
   });
-  
+});
 
-// Start the server
+const port = process.env.PORT || 5000; // Use PORT environment variable or default to 5000
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
